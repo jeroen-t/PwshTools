4d2f90f543d8822d03bc8a78c187d08a492d9a94
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2022-01-12 20:51:08 +0100

    init tls

---
+++ functions/Get-jtTlsSettings.ps1
@@ -0,0 +1,27 @@
+function Get-jtTlsSettings {
+    Param (
+        [parameter(Mandatory)]
+        [string[]]$protocol
+    )
+    $path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols'
+
+    Get-ChildItem -Path $path | Where-Object PSchildName -in $protocol | ForEach-Object {
+        $protocolpath = $_.pspath
+        'Client','Server' | ForEach-Object {
+            $fullpath = Join-Path $protocolpath $_
+            if (Test-Path $fullpath) {
+                $val = Get-ItemProperty $fullpath
+                [PSCustomObject]@{
+                    ComputerName = $env:COMPUTERNAME
+                    Protocol = $val.PSParentPath -replace '.*\\'
+                    Key = $val.PSPath -replace '.*\\'
+                    DisabledByDefault = $val.DisabledByDefault
+                    Enabled = $val.Enabled
+                }
+            }
+        }
+    }
+}
+
+$protocol = @('tls 1.2','tls 1.3')
+Invoke-Command -ScriptBlock ${function:Get-jtTlsSettings} -ArgumentList $protocol -ComputerName $computer
