8e5f3afdb84c8adfc90cbad5745266c14fc38559
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2022-02-20 20:04:10 +0100

    Add Azure Cli subscription switch wrapper function

---
+++ functions/Set-jtAzCliSubscription.ps1
@@ -0,0 +1,21 @@
+function Set-jtAzCliSubscription {
+    #Requires -Modules Microsoft.PowerShell.ConsoleGuiTools
+    #Requires -Version 7
+    # AzureCli (+ resource-graph extension) | az extension add --name resource-graph
+    $subscriptions = (az graph query -q "resourcecontainers | where type == 'microsoft.resources/subscriptions' | project name, subscriptionId, tags" | ConvertFrom-Json).data
+    if ($LASTEXITCODE) {
+        $_; EXIT
+    }
+    $subscriptionId = (($subscriptions | ForEach-Object {
+                $description = $_.tags.description ? $_.tags.description : 'N.A.'
+                $environment = $_.tags.Environment ? $_.tags.Environment : '---'
+                [PSCustomObject]@{
+                    name           = $_.Name
+                    description    = $description
+                    environment    = $environment
+                    subscriptionId = $_.subscriptionId
+                }
+            }) | Out-ConsoleGridView -OutputMode Single).subscriptionId
+    $subscriptionId ? (az account set -s $subscriptionId) : "Exited. The current active subscription is:"
+    az account show
+}
